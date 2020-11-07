c1d64c6cf67eb70528aaae012ba008a8b1f4f7da
Priya Nigam <priya.nigam@temple.edu>
2020-11-07 16:33:32 -0500

    Added naming and strength customizations

--- src/main/java/org/openjfx/Character.java
+++ src/main/java/org/openjfx/Character.java
@@ -1,5 +1,30 @@
 package org.openjfx;
 
+//The purpose of this character class is to allow user to customize their characters with options such as stats and naming
 public class Character {
+    String name; //name of character
+    int strength; //strength of character, from 1 to 100
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public Character(String name, int strength) {
+        this.name = name;
+        this.strength = strength;
+    }
+
 
 }
