20dfbb43137a40b2fe256359a240adf4877ab53a
Priya Nigam <priya.nigam@temple.edu>
2020-11-09 11:10:32 -0500

    Added getter and setter Methods for Ability Modifiers to Allow for easy access. Also,set default hitPoints at level 1 to be 15 and allowed for hitPoints to increase based on constitution modifers and hit die rolling. Added testing methods in AppTest to ensure functionality of these additions

--- src/test/java/org/openjfx/AppTest.java
+++ src/test/java/org/openjfx/AppTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AppTest {
 
@@ -25,12 +25,14 @@ public class AppTest {
     public void shouldCreateCharacter() {
         //Testing that making a character works
         Character Alex = new Character("Alex", "Human");
+        //want to make sure that random numbers are being produced
         System.out.println("The character's strength is " + Alex.getStrength());
         System.out.println("The character's dexterity is " + Alex.getDexterity());
         System.out.println("The character's constitution is " + Alex.getConstitution());
         System.out.println("The character's intelligence is " + Alex.getIntelligence());
         System.out.println("The character's wisdom is " + Alex.getWisdom());
         System.out.println("The character's charisma is " + Alex.getCharisma());
+        //Ensure that we can get Human related variables
         assertEquals("Should print Alex as name ", "Alex", Alex.getName());
         assertEquals("Should print Human as race ", "Human", Alex.getRace());
         assertEquals("Should print Common as language", "Common", Alex.getLanguage());
@@ -71,6 +73,33 @@ public class AppTest {
         assertEquals("Should print 300 as XP", 300, Alex.getXP());
         assertEquals("Should print 2 as level", 2, Alex.getLevel());
 
+    }
+
+    /**
+     * Test 4: Ensures that player starts with HP of 15.
+     */
+    @Test
+    public void shouldHave15AsDefaultHP() {
+        //Testing that making a character works
+        Character Alex = new Character("Alex", "Human");
+        assertEquals("Should print 15 as HP", 15, Alex.getHitPoints());
+    }
+
+    /**
+     * Test 5: Ensures that with an increase in 300 XP, the character levels up and has an increase in HP
+     */
+    @Test
+    public void shouldIncreaseHPWithLevelUp() {
+        //Testing that making a character works
+        Character Alex = new Character("Alex", "Human");
+        assertEquals("Should print 0 as XP", 0, Alex.getXP() );
+        assertEquals("Should print 15 as HP", 15, Alex.getHitPoints() );
+        assertEquals("Should print 1 as level", 1, Alex.getLevel() );
+        //Testing that increasing XP to next level causes increase in HP
+        Alex.addXP(300);
+        assertEquals("Should print 300 as XP", 300, Alex.getXP());
+        assertEquals("Should print 2 as level", 2, Alex.getLevel());
+        assertNotEquals("Should not be 15 as HP", 15, Alex.getHitPoints());
     }
 
 }
