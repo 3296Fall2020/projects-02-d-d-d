baa5f03839c0ddc3b8cf772b99873a2b8b9e01a8
Aszliah Delicana <aszliahdelicana@gmail.com>
2020-11-17 15:22:54 -0500

    Added mroe in-depth comments to the CombatController. Fixed a typo in the CorruptedMage class. pom.xml file was modified after importing JUnit dependencies.

--- src/main/java/org/openjfx/CombatController.java
+++ src/main/java/org/openjfx/CombatController.java
@@ -15,10 +15,12 @@ import java.util.ResourceBundle;
 
 public class CombatController extends App implements Initializable {
 
-    //the current combat
+    //a Combat variable for the current instance of combat
     public Combat combat;
 
-    //variables for the fxml file
+    /** These are the various Text "fields" that are used in the Controller's matching .fxml file (CombatController = combat.fxml)
+        They are accessed using the .getText() and setText() methods.
+        More documentation found at: http://tutorials.jenkov.com/javafx/text.html**/
     @FXML
     public Text description; //the main text for the combat screen
     public Text enemyTurnDescription; //text describing the enemy's turn
@@ -31,7 +33,8 @@ public class CombatController extends App implements Initializable {
     //a boolean that keeps track of whether the cooldown has been decreased this round already.
     private boolean cdDecreased;
 
-    //buttons on the combat screen
+    /** The buttons that are on the combat screen.
+        These are instantiated in the Controller so that they can be accessed and modified (such as setting visibility). **/
     @FXML
     Button examineButton;
     @FXML
@@ -46,9 +49,9 @@ public class CombatController extends App implements Initializable {
     //an int representing the player's healing ability cooldown (= once per 4 attacks)
     private int healCD;
 
-    //Runs when combat.fxml is first loaded. Displays the combat details, introduces the opponent, and makes
-    //sure that the "return" button to return to the main screen is not visible.
-    //Initializes the heal countdown to 0 and cdDecreased to false.
+    /** Runs when combat.fxml is first loaded. Displays the combat details, introduces the opponent, and makes
+        sure that the "return" button (to return to the main screen) is not visible.
+        Initializes the heal countdown to 0 and cdDecreased to false. **/
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         combat = new Combat(player);
@@ -59,24 +62,30 @@ public class CombatController extends App implements Initializable {
         this.cdDecreased = false;
     }
 
-    //print the updated combat details each round
+    /** Print the updated combat details each round.
+        The method combat.getCombatDetails() handles putting the player/enemy healths in Strings in the Combat class;
+        This function simply fetches that String and sets it to the combatDetails Text field in combat.fxml. **/
     private void updateCombatDetails(){
         combatDetails.setText(combat.getCombatDetails() + "\n\n");
     }
 
+    /** Print the description of the round on the screen. Replaces previous round description. **/
     private void showRoundDescription(){
         description.setText(this.roundDescription);
     }
 
+    /** Append to the round description. **/
     private void updateRoundDescription(String text){
         roundDescription += text + "\n";
     }
 
+    /** clears the description Text field. **/
     @FXML
     private void clearText(){
         description.setText("");
     }
 
+    /** Examining the opponent. Updates the roundDescription with the opponent's description. **/
     @FXML
     private void examine() throws IOException {
         String examineText = combat.examineOpponent();
@@ -84,6 +93,14 @@ public class CombatController extends App implements Initializable {
         showRoundDescription();
     }
 
+    /** Attacking the opponent. Depending on who goes first, either the player attacks and then the opponent
+        follows up with their own attack, or the opponent attacks first and then the player attacks.
+
+        Combines the output strings of each attack using updateRoundDescription(), then shows the combined
+        description with showRoundDescription().
+
+        Lastly, updates the player's heal cooldown with updateHealCD() and checks on the status of
+        combat with updateCombat().**/
     @FXML
     private void attack() throws IOException {
         roundDescription = "";
@@ -100,10 +117,14 @@ public class CombatController extends App implements Initializable {
             updateRoundDescription(playerTurnText);
         }
         showRoundDescription();
-        updateCombat();
         updateHealCD();
+        updateCombat();
     }
 
+    /** The player heals.
+        If heal is off cooldown, allow it to happen (following the turn order).
+        Otherwise, display how many more turns the player needs to wait and forces them to
+        make another choice.**/
     @FXML
     private void heal() throws IOException {
         roundDescription = "";
@@ -128,10 +149,11 @@ public class CombatController extends App implements Initializable {
         else{
             updateRoundDescription("Please wait " + this.healCD + " turns before trying to heal again!\n");
             showRoundDescription();
-
         }
     }
 
+    /** If heal is on cooldown (healCD > 0), decrement it.
+        If at 0, keep it at 0. **/
     private void updateHealCD() {
         if (this.healCD > 0) {
             this.healCD--;
@@ -140,6 +162,7 @@ public class CombatController extends App implements Initializable {
             this.healCD = 0;
     }
 
+    /** The opponent's turn. Adds their turn description to the roundDescription.**/
     @FXML
     private void opponentTurn() throws IOException{
         updateRoundDescription("It's " + combat.opponent.getName() + "'s turn!");
@@ -147,12 +170,15 @@ public class CombatController extends App implements Initializable {
         updateRoundDescription(opponentTurnText);
     }
 
+    /** Flee combat. Does not reward XP. **/
     @FXML
     private void flee() throws IOException {
         updateRoundDescription("You fled combat!");
         App.setRoot("primary");
     }
 
+    /** Check on the combat status. If someone has won (someone's HP dropped to 0), show the end screen.
+        Otherwise, update the combat details. **/
     @FXML
     private void updateCombat() throws IOException {
         if (combat.checkForWin()) {
@@ -163,6 +189,9 @@ public class CombatController extends App implements Initializable {
         }
     }
 
+    /** Shows the endscreen. The endscreen should update the description Text field with the combat's outcomeString,
+        make all other combat buttons invisible so the player can no longer access them,
+        then make the endCombatButton visible so the player can return to the previous page.**/
     @FXML
     private void showEndScreen() throws IOException {
         description.setText(combat.getOutcomeString());
@@ -174,6 +203,7 @@ public class CombatController extends App implements Initializable {
         endCombatButton.setVisible(true);
     }
 
+    /** End combat by switching "pages." (Currently switches to primary.fxml by default) **/
     @FXML
     private void endCombat() throws IOException {
         App.setRoot("primary");
