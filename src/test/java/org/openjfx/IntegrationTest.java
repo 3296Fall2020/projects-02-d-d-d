848c15fd346d141302cb10876165a41aae3b7572
regnivon <qdnovinger@gmail.com>
2020-11-21 22:58:10 -0500

    Testing for intergration services

--- src/test/java/org/openjfx/IntegrationTest.java
+++ src/test/java/org/openjfx/IntegrationTest.java
@@ -1,8 +1,17 @@
 package org.openjfx;
 
+import dnd.characters.Character;
+import dnd.characters.Dwarf;
+import dnd.characters.Elf;
 import dnd.integration.*;
+import dnd.weapons.Sword;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 public class IntegrationTest {
@@ -12,16 +21,39 @@ public class IntegrationTest {
     @Before
     public void setUp() {
         sessionManager = new SessionManager();
+        sessionManager.createUser("bobthebuilder13243", "bobspass");
+        sessionManager.authenticateSession("bobthebuilder13243", "bobspass");
+
+        List<Character> noChars = new ArrayList<>();
+        sessionManager.saveGame("nocharsgame", noChars);
+        sessionManager.saveGame("secondnocharsgame", noChars);
     }
 
     @Test
-    public void shouldCreateUser() {
+    public void shouldCreateUserAndGetToken() {
         assertTrue(sessionManager.createUser("bob", "sdfsdf"));
+        assertTrue(sessionManager.authenticateSession("bob", "sdfsdf"));
     }
 
     @Test
-    public void shouldFetchToken() {
-        assertTrue(sessionManager.authenticateSession("bob", "sdfsdf"));
+    public void shouldSaveThenGetGame() {
+        Character dwarf = new Dwarf("steve");
+        dwarf.setLevel(4);
+        dwarf.setWeapon(new Sword());
+        Character elf = new Elf("joe");
+        List<Character> toSave = new ArrayList<>();
+        toSave.add(dwarf);
+        toSave.add(elf);
+        sessionManager.saveGame("testgame", toSave);
+        List<Character> chars = sessionManager.fetchCharactersForGame("testgame");
+        assertEquals(chars.get(0), dwarf);
+        assertEquals(chars.get(1), elf);
+    }
+
+    @Test
+    public void shouldGetAllGames() {
+        List<String> games = sessionManager.fetchGamesForPlayer();
+        assertEquals(games.get(0), "nocharsgame");
     }
 
 
